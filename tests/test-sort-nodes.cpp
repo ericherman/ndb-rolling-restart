95703e7ab6f23f59a9dac0ee04aae83569a43a12
Eric Herman <eric@freesa.org>
2018-10-25 09:55:37 +0200

    fix test; still have memory issue, and surprising results

--- tests/test-sort-nodes.cpp
+++ tests/test-sort-nodes.cpp
@@ -95,6 +95,29 @@ int test_node_sorting_48(int verbose)
 
     struct restart_node_status_s expected_nodes[] = {
         //
+        { node_id : 1, node_group : 0, was_restarted : false }, //
+        { node_id : 3, node_group : 1, was_restarted : false }, //
+        { node_id : 5, node_group : 2, was_restarted : false }, //
+        { node_id : 7, node_group : 3, was_restarted : false }, //
+        { node_id : 9, node_group : 4, was_restarted : false }, //
+        { node_id : 13, node_group : 6, was_restarted : false }, //
+        { node_id : 15, node_group : 7, was_restarted : false }, //
+        { node_id : 17, node_group : 8, was_restarted : false }, //
+        { node_id : 19, node_group : 9, was_restarted : false }, //
+        { node_id : 21, node_group : 10, was_restarted : false }, //
+        { node_id : 23, node_group : 11, was_restarted : false }, //
+        { node_id : 25, node_group : 12, was_restarted : false }, //
+        { node_id : 27, node_group : 13, was_restarted : false }, //
+        { node_id : 29, node_group : 14, was_restarted : false }, //
+        { node_id : 31, node_group : 15, was_restarted : false }, //
+        { node_id : 33, node_group : 16, was_restarted : false }, //
+        { node_id : 35, node_group : 17, was_restarted : false }, //
+        { node_id : 37, node_group : 18, was_restarted : false }, //
+        { node_id : 39, node_group : 19, was_restarted : false }, //
+        { node_id : 41, node_group : 20, was_restarted : false }, //
+        { node_id : 43, node_group : 21, was_restarted : false }, //
+        { node_id : 45, node_group : 22, was_restarted : false }, //
+        { node_id : 47, node_group : 23, was_restarted : false }, //
         { node_id : 2, node_group : 0, was_restarted : false }, //
         { node_id : 4, node_group : 1, was_restarted : false }, //
         { node_id : 6, node_group : 2, was_restarted : false }, //
@@ -118,30 +141,7 @@ int test_node_sorting_48(int verbose)
         { node_id : 42, node_group : 20, was_restarted : false }, //
         { node_id : 44, node_group : 21, was_restarted : false }, //
         { node_id : 46, node_group : 22, was_restarted : false }, //
-        { node_id : 48, node_group : 23, was_restarted : false }, //
-        { node_id : 1, node_group : 0, was_restarted : false }, //
-        { node_id : 3, node_group : 1, was_restarted : false }, //
-        { node_id : 5, node_group : 2, was_restarted : false }, //
-        { node_id : 7, node_group : 3, was_restarted : false }, //
-        { node_id : 9, node_group : 4, was_restarted : false }, //
-        { node_id : 13, node_group : 6, was_restarted : false }, //
-        { node_id : 15, node_group : 7, was_restarted : false }, //
-        { node_id : 17, node_group : 8, was_restarted : false }, //
-        { node_id : 19, node_group : 9, was_restarted : false }, //
-        { node_id : 21, node_group : 10, was_restarted : false }, //
-        { node_id : 23, node_group : 11, was_restarted : false }, //
-        { node_id : 25, node_group : 12, was_restarted : false }, //
-        { node_id : 27, node_group : 13, was_restarted : false }, //
-        { node_id : 29, node_group : 14, was_restarted : false }, //
-        { node_id : 31, node_group : 15, was_restarted : false }, //
-        { node_id : 33, node_group : 16, was_restarted : false }, //
-        { node_id : 35, node_group : 17, was_restarted : false }, //
-        { node_id : 37, node_group : 18, was_restarted : false }, //
-        { node_id : 39, node_group : 19, was_restarted : false }, //
-        { node_id : 41, node_group : 20, was_restarted : false }, //
-        { node_id : 43, node_group : 21, was_restarted : false }, //
-        { node_id : 45, node_group : 22, was_restarted : false }, //
-        { node_id : 47, node_group : 23, was_restarted : false } //
+        { node_id : 48, node_group : 23, was_restarted : false } //
     };
     return test_node_sorting(nodes, 48, expected_nodes, verbose);
 }
@@ -167,7 +167,7 @@ int test_node_sorting_6(int verbose)
         { node_id : 5, node_group : 1, was_restarted : false }, //
         { node_id : 7, node_group : 2, was_restarted : false } //
     };
-    return test_node_sorting(nodes, 48, expected_nodes, verbose);
+    return test_node_sorting(nodes, 6, expected_nodes, verbose);
 }
 
 int main(int argc, char** argv)
